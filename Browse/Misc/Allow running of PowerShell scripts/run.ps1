4f806e39a3bb971dc6f53ad182a52e89da7ad446
ArranMcDougle <arran.mcdougle@gmail.com>
2025-07-31 12:25:43 +0100

    New script allowing PowerShell scripts to run without manual copy/paste

---
+++ Browse/Misc/Allow running of PowerShell scripts/run.ps1
@@ -0,0 +1,10 @@
+# Check for Admin
+If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "Not running as administrator. Restarting with elevated privileges..."
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine
+pause
